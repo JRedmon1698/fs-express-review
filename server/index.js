b8f530a60063d813929b0593f9142b29bb5e5a3f
Joshua Elder <jcelder88@gmail.com>
2020-09-08 17:31:20 -0700

    init

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const fs = require('fs');
+const path = require('path');
+
+const FILE_DIR = path.resolve(__dirname, '..', 'files');
+const FILE_PATH = path.resolve(FILE_DIR, 'file.txt');
+const PORT = 3000;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* 
+  Write a request handler that will write the request body to the file located at FILE_PATH 
+  
+  Use Postman to test. Note: you may have to change the body type in Postman to JSON
+*/
+
+app.post('/file', () => {
+  throw new Error('Fix Me')
+});
+
+/* 
+  Write a request handler that will read from the file located at FILE_PATH
+  and send the information back to the client.
+
+  Use Postman to test.
+*/
+
+app.get('/file', () => {
+  throw new Error('Fix Me')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on localhost:${PORT}`);
+})
